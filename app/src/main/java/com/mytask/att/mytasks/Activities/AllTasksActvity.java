211c38b8e5502b365aa66d7884cd3942dfe73661
sagimiz <36265074+sagimiz@users.noreply.github.com>
2018-12-16 22:54:18 +0200

    Initial commit

---
+++ app/src/main/java/com/mytask/att/mytasks/Activities/AllTasksActvity.java
@@ -0,0 +1,15 @@
+package com.mytask.att.mytasks.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mytask.att.mytasks.R;
+
+public class AllTasksActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_tasks_actvity);
+    }
+}
